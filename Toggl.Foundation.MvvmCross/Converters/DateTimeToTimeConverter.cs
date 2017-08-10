e0f5707144be83c1c10dcef0e45612b5cae63259
Arturs Eimanis <arturseimanis@gmail.com>
2017-08-10 16:08:00 +0300

    :construction: Add time converter

---
+++ Toggl.Foundation.MvvmCross/Converters/DateTimeToTimeConverter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using MvvmCross.Platform.Converters;
+
+namespace Toggl.Foundation.MvvmCross.Converters
+{
+    public class DateTimeToTimeConverter : MvxValueConverter<DateTime, string>
+    {
+        protected override string Convert(DateTime value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+            => value.ToString("t");
+    }
+}
