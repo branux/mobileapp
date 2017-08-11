f339dc7471b3ffb7b4b46de3e0f54fdb3f8885f5
Arturs Eimanis <arturseimanis@gmail.com>
2017-08-11 15:09:58 +0300

    :construction: Use DateTimeOffset in DateTimeToTimeConverter

--- Toggl.Foundation.MvvmCross/Converters/DateTimeToTimeConverter.cs
+++ Toggl.Foundation.MvvmCross/Converters/DateTimeToTimeConverter.cs
@@ -3,9 +3,9 @@ using MvvmCross.Platform.Converters;
 
 namespace Toggl.Foundation.MvvmCross.Converters
 {
-    public class DateTimeToTimeConverter : MvxValueConverter<DateTime, string>
+    public class DateTimeToTimeConverter : MvxValueConverter<DateTimeOffset, string>
     {
-        protected override string Convert(DateTime value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        protected override string Convert(DateTimeOffset value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
             => value.ToString("t");
     }
 }
